ce3647c378c8f7c8ee18bd8647dde76629713ed9
Ryan Hefner <ryan.hefner@gooddata.com>
2025-08-26 09:59:04 +0000

    fix: ecr pullthrough race condition and sizing

--- modules/k8s-common/variables.tf
+++ modules/k8s-common/variables.tf
@@ -11,6 +11,11 @@ variable "helm_metrics_server_version" { type = string }
 variable "helm_gdcn_version" { type = string }
 variable "helm_pulsar_version" { type = string }
 
+variable "gdcn_replica_count" {
+  description = "Number of replicas of GoodData.CN microserices to run"
+  type        = number
+}
+
 variable "ingress_ip" { type = string }
 variable "db_hostname" { type = string }
 variable "db_username" { type = string }
