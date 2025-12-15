d1063d9cebf41155291c20ca263d196ce503b902
Ryan Hefner <ryan.hefner@gooddata.com>
2025-12-15 15:39:57 +0000

    refactor aws providers

--- aws/providers.tf
+++ aws/providers.tf
@@ -42,14 +42,12 @@ provider "aws" {
   }
 }
 
-# Kubernetes and Helm providers will connect to the EKS cluster using its API endpoint and token.
-# These values are obtained from the EKS cluster data (set up in eks-cluster.tf).
-provider "kubernetes" {
-  host                   = module.eks.cluster_endpoint
-  cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
-
-  exec {
-    api_version = "client.authentication.k8s.io/v1beta1"
+locals {
+  # Shared Kubernetes auth settings (aws eks get-token)
+  kube_host = module.eks.cluster_endpoint
+  kube_ca   = base64decode(module.eks.cluster_certificate_authority_data)
+  eks_exec = {
+    api_version = "client.authentication.k8s.io/v1"
     command     = "aws"
     args = [
       "eks", "get-token",
@@ -57,40 +55,43 @@ provider "kubernetes" {
       "--region", var.aws_region,
       "--profile", var.aws_profile_name
     ]
+  }
+}
+
+# Kubernetes and Helm providers will connect to the EKS cluster using its API endpoint and token.
+# These values are obtained from the EKS cluster data (set up in eks-cluster.tf).
+provider "kubernetes" {
+  host                   = local.kube_host
+  cluster_ca_certificate = local.kube_ca
+
+  exec {
+    api_version = local.eks_exec.api_version
+    command     = local.eks_exec.command
+    args        = local.eks_exec.args
   }
 }
 
 provider "helm" {
   kubernetes = {
-    host                   = module.eks.cluster_endpoint
-    cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
+    host                   = local.kube_host
+    cluster_ca_certificate = local.kube_ca
     load_config_file       = false
 
     exec = {
-      api_version = "client.authentication.k8s.io/v1beta1"
-      command     = "aws"
-      args = [
-        "eks", "get-token",
-        "--cluster-name", module.eks.cluster_name,
-        "--region", var.aws_region,
-        "--profile", var.aws_profile_name
-      ]
+      api_version = local.eks_exec.api_version
+      command     = local.eks_exec.command
+      args        = local.eks_exec.args
     }
   }
 }
 
 provider "kubectl" {
-  host                   = module.eks.cluster_endpoint
-  cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
+  host                   = local.kube_host
+  cluster_ca_certificate = local.kube_ca
   load_config_file       = false
   exec {
-    api_version = "client.authentication.k8s.io/v1beta1"
-    command     = "aws"
-    args = [
-      "eks", "get-token",
-      "--cluster-name", module.eks.cluster_name,
-      "--region", var.aws_region,
-      "--profile", var.aws_profile_name
-    ]
+    api_version = local.eks_exec.api_version
+    command     = local.eks_exec.command
+    args        = local.eks_exec.args
   }
 }
