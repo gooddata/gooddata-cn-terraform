ebe7a1809b03f2fd7594a78dec472f8c792b9863
GD Renovate <gdrenovate@gooddata.com>
2026-03-16 10:28:26 +0000

    chore(deps): update helm release loki to v6.55.0
    risk: low

--- aws/variables.tf
+++ aws/variables.tf
@@ -258,7 +258,7 @@ variable "helm_loki_version" {
   description = "Version of the loki Helm chart to deploy. https://artifacthub.io/packages/helm/grafana/loki"
   type        = string
   # renovate: depName=loki registryUrl=https://grafana.github.io/helm-charts
-  default = "6.30.1"
+  default = "6.55.0"
 }
 
 variable "helm_metrics_server_version" {
