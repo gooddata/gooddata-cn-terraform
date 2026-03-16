8d4c7a53c4e2b178749ee12966ed832f7db1f4c6
GD Renovate <gdrenovate@gooddata.com>
2026-03-16 11:54:49 +0000

    chore(deps): update helm release kube-prometheus-stack to v82
    risk: high

--- aws/variables.tf
+++ aws/variables.tf
@@ -272,7 +272,7 @@ variable "helm_prometheus_version" {
   description = "Version of the prometheus Helm chart to deploy. https://artifacthub.io/packages/helm/prometheus-community/prometheus"
   type        = string
   # renovate: depName=kube-prometheus-stack registryUrl=https://prometheus-community.github.io/helm-charts
-  default = "27.39.0"
+  default = "82.10.4"
 }
 
 variable "helm_promtail_version" {
