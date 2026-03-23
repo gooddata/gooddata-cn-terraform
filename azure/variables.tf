f65ed2748d44074ffba5103c451a4f0d0b78be3c
GD Renovate <gdrenovate@gooddata.com>
2026-03-23 12:04:21 +0000

    chore(deps): update helm release kube-prometheus-stack to v67.11.0
    risk: low

--- azure/variables.tf
+++ azure/variables.tf
@@ -236,7 +236,7 @@ variable "helm_kube_prometheus_stack_version" {
   description = "Version of the kube-prometheus-stack Helm chart to deploy. https://artifacthub.io/packages/helm/prometheus-community/kube-prometheus-stack"
   type        = string
   # renovate: depName=kube-prometheus-stack registryUrl=https://prometheus-community.github.io/helm-charts
-  default = "67.9.0"
+  default = "67.11.0"
 }
 
 variable "helm_promtail_version" {
