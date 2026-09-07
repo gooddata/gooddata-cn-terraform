0f99029512afe82b441b096ce4e6ceb6bf425915
GD Renovate <gdrenovate@gooddata.com>
2026-09-07 11:38:12 +0000

    chore(deps): update helm release langfuse to v2
    risk: high

--- aws/variables.tf
+++ aws/variables.tf
@@ -360,7 +360,7 @@ variable "helm_langfuse_version" {
   description = "Version of the langfuse Helm chart to deploy. https://artifacthub.io/packages/helm/langfuse/langfuse"
   type        = string
   # renovate: depName=langfuse registryUrl=https://langfuse.github.io/langfuse-k8s
-  default = "1.5.41"
+  default = "2.1.0"
 }
 
 variable "helm_loki_version" {
