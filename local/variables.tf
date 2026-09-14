cb28791f344cc5c5c86db4263b780058205e0c92
GD Renovate <gdrenovate@gooddata.com>
2026-09-14 07:51:15 +0000

    chore(deps): update helm release prometheus-operator-crds to v32
    risk: high

--- local/variables.tf
+++ local/variables.tf
@@ -202,7 +202,7 @@ variable "helm_prometheus_operator_crds_version" {
   description = "Version of the prometheus-operator-crds Helm chart. Must match the prometheus-operator version bundled in helm_kube_prometheus_stack_version."
   type        = string
   # renovate: depName=prometheus-operator-crds registryUrl=https://prometheus-community.github.io/helm-charts
-  default = "31.0.1"
+  default = "32.0.0"
 }
 
 variable "helm_promtail_version" {
