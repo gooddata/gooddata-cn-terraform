17c51bfb1a3d87dc372994e35a42fd4b9a0b402e
GD Renovate <gdrenovate@gooddata.com>
2026-09-14 13:17:57 +0000

    chore(deps): update helm release kube-prometheus-stack to v91
    risk: high

--- local/variables.tf
+++ local/variables.tf
@@ -195,7 +195,7 @@ variable "helm_kube_prometheus_stack_version" {
   description = "Version of the kube-prometheus-stack Helm chart to deploy."
   type        = string
   # renovate: depName=kube-prometheus-stack registryUrl=https://prometheus-community.github.io/helm-charts
-  default = "90.0.0"
+  default = "91.3.0"
 }
 
 variable "helm_prometheus_operator_crds_version" {
